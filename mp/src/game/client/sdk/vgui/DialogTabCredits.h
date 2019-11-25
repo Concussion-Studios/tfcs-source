31e71bb9203af24fe642f5d83ef02d9010dced6e
Kris The Human <nikodash2020@hotmail.com>
2019-11-24 19:45:05 -0500

    What's New UI

---
+++ mp/src/game/client/sdk/vgui/DialogTabCredits.h
@@ -0,0 +1,35 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================//
+#ifndef DIALOGTABCREDITS_H
+#define DIALOGTABCREDITS_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "vgui_controls/PropertyPage.h"
+#include <vgui_controls/RichText.h>
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+class CDialogTabCredits : public vgui::PropertyPage
+{
+	DECLARE_CLASS_SIMPLE( CDialogTabCredits, vgui::PropertyPage );
+
+public:
+	CDialogTabCredits( vgui::Panel *parent );
+	~CDialogTabCredits( void );
+
+	virtual void OnCommand( const char *command );
+	virtual void OnClose( void );
+
+private:
+
+	vgui::RichText* m_RichText;
+};
+
+#endif // DIALOGTABCREDITS_H
